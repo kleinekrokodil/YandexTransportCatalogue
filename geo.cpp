d6d16009a1544dc51af734898c1717360a314a8d
Vladimir Kibirev <kaiservladimir@gmail.com>
2022-01-31 01:15:44 +0800

    Радиус земли вынесен в константу

--- geo.cpp
+++ geo.cpp
@@ -8,9 +8,10 @@ namespace geo {
     double ComputeDistance(Coordinates from, Coordinates to) {
         using namespace std;
         const double dr = M_PI / 180.0;
+        const int earth_radius = 6371000;
         return acos(sin(from.lat * dr) * sin(to.lat * dr)
                     + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-               * 6371000;
+               * earth_radius;
     }
 
 }  // namespace geo
